112771517dd4f92e152fa7200ec03bf69cf43974
ahmedX999 <aaittaleb.354@gmail.com>
2023-03-30 18:19:07 +0100

    new repository

---
+++ src/main/java/ma/project/configs/webconfig.java
@@ -0,0 +1,18 @@
+package ma.project.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class webconfig implements WebMvcConfigurer {
+
+	
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	            .allowedOrigins("*")
+	            .allowedMethods("GET", "POST", "PUT", "DELETE")
+	            .allowCredentials(false)
+	            .maxAge(3600);
+	    }
+}
---
+++ src/main/java/ma/project/entities/Field.java
@@ -0,0 +1,71 @@
+package ma.project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Field {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+    //nom du terrain
+	private String name;
+	
+	//reserve ou non reserve  ou pas valable pour le moment
+	private int state;
+	
+	//capacite de joueur soit 5 par equipe  soit 7 par equipe
+	private int capacity;
+	
+	//prix de reservation pour chaque personne
+	private int price_perslot;
+	
+	public Field() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getPrice_perslot() {
+		return price_perslot;
+	}
+	public void setPrice_perslot(int price_perslot) {
+		this.price_perslot = price_perslot;
+	}
+	
+	
+	
+
+	
+
+	
+	
+
+
+}
